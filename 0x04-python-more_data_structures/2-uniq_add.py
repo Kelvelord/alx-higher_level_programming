342bd5493400bea4da3e04dd138598b5e352827e
me <kelvelord80@gmail.com>
2023-01-11 04:10:44 -0800

    task done

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-    if my_list:
-        black_list = []
-        som = 0
-        for n in my_list:
-            if n not in black_list:
-                som += n
-                black_list.append(n)
-        return(som)
+    number = 0
+    for element in set(my_list):
+        number += element
+    return number
